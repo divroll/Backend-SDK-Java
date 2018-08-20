ceffc3a86178877f6750b5da1db83440e4f1cc96
kerby <kerby@dotweblabs.com>
2018-08-20 12:53:02 +0000

    Test for retrieve Users with ACL

---
+++ src/main/java/com/divroll/domino/DominoUsers.java
@@ -0,0 +1,194 @@
+package com.divroll.domino;
+
+import com.divroll.domino.exception.BadRequestException;
+import com.divroll.domino.exception.UnauthorizedException;
+import com.divroll.domino.helper.JSON;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.GetRequest;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DominoUsers extends DominoBase {
+
+    private static final String usersUrl = "/entities/users";
+
+    private List<DominoUser> users;
+    private int skip = 0;
+    private int limit = 100;
+
+    public List<DominoUser> getUsers() {
+        if(users == null) {
+            users = new LinkedList<DominoUser>();
+        }
+        return users;
+    }
+
+    public void setUsers(List<DominoUser> users) {
+        this.users = users;
+    }
+
+    public int getSkip() {
+        return skip;
+    }
+
+    public void setSkip(int skip) {
+        this.skip = skip;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public void query() {
+        try {
+            String completeUrl = Domino.getServerUrl() + usersUrl;
+            System.out.println(completeUrl);
+            GetRequest getRequest = (GetRequest) Unirest.get(completeUrl);
+
+            if(Domino.getMasterKey() != null) {
+                getRequest.header("X-Domino-Master-Key", Domino.getMasterKey());
+            }
+            if(Domino.getAppId() != null) {
+                getRequest.header("X-Domino-App-Id", Domino.getAppId());
+            }
+            if(Domino.getApiKey() != null) {
+                getRequest.header("X-Domino-Api-Key", Domino.getApiKey());
+            }
+            if(Domino.getAuthToken() != null) {
+                getRequest.header("X-Domino-Auth-Token", Domino.getAuthToken());
+            }
+
+            HttpResponse<JsonNode> response = getRequest.asJson();
+            System.out.println(response.getBody().toString());
+
+            if(response.getStatus() >= 500) {
+                throwException(response);
+            } else if(response.getStatus() == 401) {
+                throw new UnauthorizedException(response.getStatusText());
+            } else if(response.getStatus() == 400) {
+                throw new BadRequestException(response.getStatusText());
+            }  else if(response.getStatus() >= 400) {
+                throwException(response);
+            } else if(response.getStatus() == 200) {
+
+                JsonNode body = response.getBody();
+
+                System.out.println("HERE: " + body.toString());
+
+                JSONObject bodyObj = body.getObject();
+                JSONObject roles = bodyObj.getJSONObject("users");
+                JSONArray results = new JSONArray();
+
+                try {
+                    results = roles.getJSONArray("results");
+                } catch (Exception e) {
+
+                }
+
+                try {
+                    JSONObject jsonObject = roles.getJSONObject("results");
+                    results.put(jsonObject);
+                } catch (Exception e) {
+
+                }
+
+                for(int i=0;i<results.length();i++){
+                    JSONObject userObj = results.getJSONObject(i);
+                    String entityId = userObj.getString("entityId");
+                    String username = userObj.getString("username");
+                    Boolean publicRead = userObj.getBoolean("publicRead");
+                    Boolean publicWrite = userObj.getBoolean("publicWrite");
+
+                    List<String> aclWriteList = null;
+                    List<String> aclReadList = null;
+
+                    try {
+                        aclWriteList = JSON.toList(userObj.getJSONArray("aclWrite"));
+                    } catch (Exception e) {
+
+                    }
+
+                    try {
+                        aclReadList = JSON.toList(userObj.getJSONArray("aclRead"));
+                    } catch (Exception e) {
+
+                    }
+
+                    try {
+                        aclWriteList = Arrays.asList(userObj.getString("aclWrite"));
+                    } catch (Exception e) {
+
+                    }
+
+                    try {
+                        aclReadList = Arrays.asList(userObj.getString("aclRead"));
+                    } catch (Exception e) {
+
+                    }
+
+                    JSONArray userRoles = null;
+                    try {
+                        userRoles = userObj.getJSONArray("roles");
+                    } catch (Exception e) {
+
+                    }
+
+                    List<DominoRole> dominoRoles = null;
+                    try {
+                        if(userRoles != null) {
+                            Object roleObjects = userObj.get("roles");
+                            if(roleObjects instanceof JSONArray) {
+                                dominoRoles = new LinkedList<DominoRole>();
+                                for(int j=0;j<userRoles.length();j++) {
+                                    JSONObject jsonObject = userRoles.getJSONObject(j);
+                                    String roleId = jsonObject.getString("entityId");
+                                    DominoRole dominoRole = new DominoRole();
+                                    dominoRole.setEntityId(roleId);
+                                    dominoRoles.add(dominoRole);
+                                }
+                            } else if(roleObjects instanceof JSONObject) {
+                                dominoRoles = new LinkedList<DominoRole>();
+                                JSONObject jsonObject = (JSONObject) roleObjects;
+                                String roleId = jsonObject.getString("entityId");
+                                DominoRole dominoRole = new DominoRole();
+                                dominoRole.setEntityId(roleId);
+                                dominoRoles.add(dominoRole);
+                            }
+                        }
+
+                    } catch (Exception e) {
+                        // do nothing
+                    }
+
+                    DominoACL acl = new DominoACL(aclReadList, aclWriteList);
+                    acl.setPublicWrite(publicWrite);
+                    acl.setPublicRead(publicRead);
+
+                    DominoUser user = new DominoUser();
+                    user.setEntityId(entityId);
+                    user.setAcl(acl);
+                    user.setRoles(dominoRoles);
+                    user.setUsername(username);
+
+                    getUsers().add(user);
+
+                }
+                //System.out.println(bodyObj.toString());
+
+            }
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/test/java/com/divroll/domino/TestDominoUsers.java
+++ src/test/java/com/divroll/domino/TestDominoUsers.java
@@ -1,13 +1,132 @@
 package com.divroll.domino;
 
 import junit.framework.TestCase;
+import org.fluttercode.datafactory.impl.DataFactory;
+import org.junit.Assert;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
 
 @RunWith(JUnit4.class)
 public class TestDominoUsers extends TestCase {
     @Rule
     public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void testGetApplication() {
+        TestApplication application = TestData.getNewApplication();
+        Domino.initialize(application.getAppId(), application.getApiToken(), application.getMasterKey());
+        Assert.assertNotNull(application.getApiToken());
+        Assert.assertNotNull(application.getAppId());
+        Assert.assertNotNull(application.getMasterKey());
+    }
+
+    @Test
+    public void testGetUsers() {
+        System.out.println("Running testGetUsers");
+        TestApplication application = TestData.getNewApplication();
+        Domino.initialize(application.getAppId(), application.getApiToken(), application.getMasterKey());
+
+        DataFactory df = new DataFactory();
+        for(int i=0;i<100;i++) {
+            DominoUser dominoUser = new DominoUser();
+            dominoUser.create(df.getEmailAddress(), "password");
+        }
+
+        DominoUsers users = new DominoUsers();
+        users.query();
+
+        Assert.assertEquals(100, users.getUsers().size());
+    }
+
+    @Test
+    public void testGetUsersMasterKeyOnly() {
+        TestApplication application = TestData.getNewApplication();
+        Domino.initialize(application.getAppId(), application.getApiToken());
+
+        DataFactory df = new DataFactory();
+        for(int i=0;i<100;i++) {
+            DominoUser dominoUser = new DominoUser();
+            dominoUser.setAcl(DominoACL.buildMasterKeyOnly());
+            dominoUser.create(df.getEmailAddress(), "password");
+        }
+
+        DominoUsers users = new DominoUsers();
+        users.query();
+
+        Assert.assertEquals(0, users.getUsers().size());
+    }
+
+    @Test
+    public void testGetUsersWithACLUsingAuthToken() {
+        TestApplication application = TestData.getNewApplication();
+        Domino.initialize(application.getAppId(), application.getApiToken());
+
+        DataFactory df = new DataFactory();
+
+        DominoUser admin = new DominoUser();
+        admin.setAcl(DominoACL.buildMasterKeyOnly());
+        String adminUsername = df.getEmailAddress();
+        admin.create(adminUsername, "password");
+
+        int size = 10;
+        for(int i=0;i<size;i++) {
+            DominoUser dominoUser = new DominoUser();
+            DominoACL acl = new DominoACL();
+            acl.setPublicWrite(false);
+            acl.setPublicRead(false);
+            acl.setAclRead(Arrays.asList(admin.getEntityId()));
+            acl.setAclWrite(Arrays.asList(admin.getEntityId()));
+            dominoUser.setAcl(acl);
+            dominoUser.create(df.getEmailAddress(), "password");
+        }
+
+        DominoUsers users = new DominoUsers();
+        users.query();
+
+        Assert.assertEquals(0, users.getUsers().size());
+
+        admin.login(adminUsername, "password");
+
+        users = new DominoUsers();
+        users.query();
+
+        Assert.assertEquals(size, users.getUsers().size());
+
+        size = 20;
+        for(int i=0;i<size;i++) {
+            DominoUser dominoUser = new DominoUser();
+            DominoACL acl = new DominoACL();
+            acl.setPublicWrite(false);
+            acl.setPublicRead(true);
+            dominoUser.setAcl(acl);
+            dominoUser.create(df.getEmailAddress(), "password");
+        }
+
+        users = new DominoUsers();
+        users.query();
+
+        Assert.assertEquals(30, users.getUsers().size());
+
+        size = 20;
+        for(int i=0;i<size;i++) {
+            DominoUser dominoUser = new DominoUser();
+            DominoACL acl = new DominoACL();
+            acl.setPublicWrite(true);
+            acl.setPublicRead(false);
+            dominoUser.setAcl(acl);
+            dominoUser.create(df.getEmailAddress(), "password");
+        }
+
+        users = new DominoUsers();
+        users.query();
+
+        Assert.assertEquals(30, users.getUsers().size());
+
+    }
+
 }
